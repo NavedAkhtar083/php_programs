c45198bd2afdbb48b069658526635acdd7fe18af
NavedAkhtar083 <navedkhan9410671292@gmail.com>
2023-11-29 00:50:14 +0530

    first commit

---
+++ php_add_html.php
@@ -0,0 +1,11 @@
+<?php 
+echo "<h1 style='color:red'>hello php-11 html heading 1</h1>";
+?>
+<?php 
+$name="anil shah";
+echo "<h1> $name</h1>";
+?>
+
+<h1 style="color:pink">
+  my name is  <?php echo "$name"; ?>
+</h1>
